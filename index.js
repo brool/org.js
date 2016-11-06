e00eda10b5216933d04eddad734898fe30dd57bd
m31271n <m31271n@gmail.com>
2016-11-06 21:32:29 +0800

    Adjust api

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-function exportModule(module) {
-  for (var exportedName in module) {
-    if (module.hasOwnProperty(exportedName)) {
-      exports[exportedName] = module[exportedName];
-    }
-  }
-}
+const Parser = require('./lib/parser.js');
+const ConverterHTML = require('./lib/converter/html.js');
 
-exportModule(require("./lib/parser.js"));
-exportModule(require("./lib/lexer.js"));
-exportModule(require("./lib/node.js"));
-exportModule(require("./lib/parser.js"));
-exportModule(require("./lib/stream.js"));
-exportModule(require("./lib/converter/html.js"));
+module.exports = {
+  Parser,
+  ConverterHTML
+};
