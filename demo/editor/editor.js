adb39e4c405816ca879bf155fb53f924b21a2e80
m31271n <m31271n@gmail.com>
2016-11-06 15:57:00 +0800

    Move stub/editor to demo

---
+++ demo/editor/editor.js
@@ -0,0 +1,42 @@
+function load() {
+  var inputEl = document.getElementById("input");
+  var resultEl = document.getElementById("result");
+
+  var orgParser = new Org.Parser();
+  function renderHTML(org) {
+    //  render html from org
+    try {
+      resultEl.innerHTML = orgParser.parse(org)
+      .convert(Org.ConverterHTML, {
+        translateSymbolArrow: true
+      }).toString();
+    } catch (e) {
+      return alert(e);
+    }
+  }
+
+  function render() {
+    var org = inputEl.value;
+    console.log("somo" + org);
+    renderHTML(org);
+  }
+
+  function saveToLocalStorage() {
+    var org = inputEl.value;
+    localStorage["org"] = org;
+  }
+
+  function restoreFromLocaleStorage() {
+    var org = localStorage["org"];
+    if (org) {
+      inputEl.value = org;
+      renderHTML(org);
+    }
+  }
+
+  inputEl.addEventListener("input", render, false);
+  setInterval(saveToLocalStorage, 3000);
+  restoreFromLocaleStorage();
+}
+
+window.addEventListener("load", load, false);
