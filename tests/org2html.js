6a45a26b13e6b7d15580de386c50625cacf46656
m31271n <m31271n@gmail.com>
2016-11-13 10:55:09 +0800

    Move stub into tests

--- tests/org2html.js
+++ tests/org2html.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const org = require('./');
+const path = require('path');
+const org = require('../dist/org.cjs.js');
 
-const orgCode = fs.readFileSync('./stub/test.org', {
+const orgCode = fs.readFileSync(path.join(__dirname, 'test.org'), {
   encoding: 'utf8'
 });
 
 function parseAndOutputHTML(orgCode) {
   const parser = new org.Parser();
   const orgDoc = parser.parse(orgCode);
-  console.log(orgDoc);
   var orgHTMLDoc = orgDoc.convert(org.Converter.html, {});
-  return orgHTMLDoc.toString();
+  return orgHTMLDoc;
 }
-
-
 console.log(parseAndOutputHTML(orgCode));
